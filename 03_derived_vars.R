25416530da45b5f7d3b5d05e4c59d848156d0695
Yao Siyuan <siyuan.yao@msxf.com>
2017-11-15 11:56:50 +0800

    code framework

--- 03_derived_vars.R
+++ 03_derived_vars.R
@@ -1,13 +1,16 @@
 library(compiler)
+library(lubridate)
+
 setkey(login, id)
 setorder(login, id, -timestamp)
 setkey(trade, id)
 setorder(trade, id, -timestamp)
 ## logging vars
-derived_vars <- Vectorize(cmpfun(row_key) {
+derived_vars <- cmpfun(Vectorize(function(row_key) {
     trade_info <- trade[rowkey == row_key]
 
-    ### last login
-    login_info <- login[id == trade_info$id]
+    ### historical login
+    login_info <- login[id == trade_info$id &
+                        timestamp <= trade_info$timestamp]
 
-})
+}))
